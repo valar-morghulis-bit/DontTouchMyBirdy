dc66de068ec9fc01aeba890665d2df33bc222703
valar-morghulis-bit <jasonramirez@student.laverdad.edu.ph>
2021-03-02 23:52:31 +0800

    Added auth

--- App.js
+++ App.js
@@ -1,8 +1,23 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, Dimensions, TouchableWithoutFeedback } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, TouchableWithoutFeedback, TextInput, Button, LogBox } from 'react-native';
 import Bird from './components/Bird'
 import Obstacles from './components/Obstacles'
 import * as Network from 'expo-network';
+import * as firebase from 'firebase'
+
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBryU57U8L8ktPYgwXvNhlfq1eitKbS3iU",
+  authDomain: "flappybird-ac50b.firebaseapp.com",
+  databaseURL: "https://flappybird-ac50b-default-rtdb.firebaseio.com",
+  projectId: "flappybird-ac50b",
+  storageBucket: "flappybird-ac50b.appspot.com",
+  messagingSenderId: "763652029007",
+  appId: "1:763652029007:web:bd63445dc0e5c7a7ae083a"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 export default function App() {
   const screenWidth = Dimensions.get("screen").width
@@ -15,6 +30,12 @@ export default function App() {
   const [obstaclesNegHeightTwo, setObstaclesNegHeightTwo]= useState(0)
   const [isGameOver, setIsGameOver]= useState(false)
   const [score, setScore]= useState(0)
+  const [registerState, setRegState] = useState({
+    showNav: true,
+    showMsg: false,
+    color: "pink",
+    msg: "",
+  });
   const gravity = 3
   let obstacleWidth = 60
   let obstacleHeight = 300
@@ -22,85 +43,265 @@ export default function App() {
   let gameTimerId
   let obstaclesTimerId
   let obstaclesTimerIdTwo
-
-  async function getMac() {
-    const gg = await Network.getMacAddressAsync("wlan0");
-    console.log(gg);
-  }
+  let userName = null
 
   useEffect(() => {
-    getMac();
+    LogBox.ignoreLogs(['Setting a timer']);
+    tryLogin();
   }, []);
   
-//start bird falling
-  useEffect(() => {
-    if (birdBottom > 0) {
-      gameTimerId = setInterval(() => {
-        setBirdBottom(birdBottom => birdBottom - gravity)
-      },30)
-  
-      return () => {
-        clearInterval(gameTimerId)
-      }
-    }
-    //if i dont have birdBottom as a dependecy, it wont stop
-  }, [birdBottom])
-
-  const jump = () => {
-    if (!isGameOver && (birdBottom < screenHeight)) {
-      setBirdBottom(birdBottom => birdBottom + 50)
+  async function tryLogin() {
+    setRegState({
+      ...registerState,
+      showMsg: true,
+      color: "orange",
+      msg: "Dude Loading..."
+    })
+    const macAddress = await Network.getMacAddressAsync("wlan0");
+    if (macAddress) {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword("doesnt@matter.com", macAddress)
+        .then(() => {
+          setRegState({
+            showNav: false,
+            showMsg: true,
+            color: "yellow",
+            msg: "Login Success",
+          });
+        })
+        .catch((err) => {
+          setRegState({
+            ...registerState,
+            showMsg: true,
+            color: "pink",
+            msg: "You are not registered honey",
+          })
+        });
     }
   }
 
-  //start first obstacle
-  useEffect(() => {
-    if (obstaclesLeft > -60) {
-      obstaclesTimerId = setInterval(() => {
-        setObstaclesLeft(obstaclesLeft => obstaclesLeft - 5)
-      }, 30)
-      return () => {
-        clearInterval(obstaclesTimerId)
-      }
-    } else {
-      setScore(score => score +1)
-      setObstaclesLeft(screenWidth)
-      setObstaclesNegHeight( - Math.random() * 100)
-    }
-  }, [obstaclesLeft])
+  async function register() {
+    const macAddress = await Network.getMacAddressAsync("wlan0");
+    userName
+    ? firebase
+      .database()
+      .ref('users')
+      .orderByChild("userName")
+      .equalTo(userName)
+      .once("value")
+      .then(snapshot => {
+        if (!snapshot.exists() && (userName !== "") && !userName.includes(" ") && macAddress) {// if null then unique
+          firebase
+          .auth()
+          .createUserWithEmailAndPassword("doesnt@matter.com", macAddress)
+          .then((arg) => {
+            const user = {
+              userName: userName,
+              highestScore: 0,
+            };
+            firebase
+              .database()
+              .ref('users/' + arg.user?.uid)
+              .update(user)
+              .then(snapshot => {
+                setRegState({
+                  ...registerState,
+                  showNav: false
+                })
+              })
+              .catch(err => {
+                console.log("SIGN UP FAILED", err);
+              })
+          })
+          .catch((err) => {
+            console.log(err)
+          });
+        } else {
+          setRegState({
+            ...registerState,
+            showMsg: true,
+            color: "pink",
+            msg: "Maybe user name already exist honey",
+          })
+        }
+      })
+      .catch((err) => console.log(err))
+    : setRegState({
+        ...registerState,
+        showMsg: true,
+        color: "pink",
+        msg: "Invalid user name honey",
+      })
+  }
 
-  //start second obstacle
-  useEffect(() => {
-    if (obstaclesLeftTwo > -60) {
-      obstaclesTimerIdTwo = setInterval(() => {
-        setObstaclesLeftTwo(obstaclesLeftTwo => obstaclesLeftTwo - 5)
-      }, 30)
-        return () => {
-          clearInterval(obstaclesTimerIdTwo)
+
+
+
+
+
+    //  //start bird falling
+    //  useEffect(() => {
+    //   if (birdBottom > 0) {
+    //     gameTimerId = setInterval(() => {
+    //       setBirdBottom(birdBottom => birdBottom - gravity)
+    //     },30)
+    
+    //     return () => {
+    //       clearInterval(gameTimerId)
+    //     }
+    //   }
+    //   //if i dont have birdBottom as a dependecy, it wont stop
+    // }, [birdBottom])
+
+    // const jump = () => {
+    //   if (!isGameOver && (birdBottom < screenHeight)) {
+    //     setBirdBottom(birdBottom => birdBottom + 50)
+    //   }
+    // }
+
+    // //start first obstacle
+    // useEffect(() => {
+    //   if (obstaclesLeft > -60) {
+    //     obstaclesTimerId = setInterval(() => {
+    //       setObstaclesLeft(obstaclesLeft => obstaclesLeft - 5)
+    //     }, 30)
+    //     return () => {
+    //       clearInterval(obstaclesTimerId)
+    //     }
+    //   } else {
+    //     setScore(score => score +1)
+    //     setObstaclesLeft(screenWidth)
+    //     setObstaclesNegHeight( - Math.random() * 100)
+    //   }
+    // }, [obstaclesLeft])
+
+    // //start second obstacle
+    // useEffect(() => {
+    //   if (obstaclesLeftTwo > -60) {
+    //     obstaclesTimerIdTwo = setInterval(() => {
+    //       setObstaclesLeftTwo(obstaclesLeftTwo => obstaclesLeftTwo - 5)
+    //     }, 30)
+    //       return () => {
+    //         clearInterval(obstaclesTimerIdTwo)
+    //       }
+    //     } else {
+    //         setScore(score => score +1)
+    //         setObstaclesLeftTwo(screenWidth)
+    //         setObstaclesNegHeightTwo( - Math.random() * 100)
+    //       }
+    // }, [obstaclesLeftTwo])
+
+    // //check for collisions
+    // useEffect(() => {
+    //   if (
+    //     ((birdBottom < (obstaclesNegHeight + obstacleHeight + 30) ||
+    //     birdBottom > (obstaclesNegHeight + obstacleHeight + gap -30)) &&
+    //     (obstaclesLeft > screenWidth/2 -30 && obstaclesLeft < screenWidth/2 + 30 )
+    //     )
+    //     || 
+    //     ((birdBottom < (obstaclesNegHeightTwo + obstacleHeight + 30) ||
+    //     birdBottom > (obstaclesNegHeightTwo + obstacleHeight + gap -30)) &&
+    //     (obstaclesLeftTwo > screenWidth/2 -30 && obstaclesLeftTwo < screenWidth/2 + 30 )
+    //     )
+    //     ) 
+    //     {
+    //     gameOver()
+    //   }
+    // })
+
+
+
+
+
+
+
+
+
+    //start bird falling
+    useEffect(() => {
+      if (!registerState.showNav) {
+        if (birdBottom > 0) {
+          gameTimerId = setInterval(() => {
+            setBirdBottom(birdBottom => birdBottom - gravity)
+          },30)
+
+          return () => {
+            clearInterval(gameTimerId)
+          }
         }
-      } else {
+        //if i dont have birdBottom as a dependecy, it wont stop
+      }
+    }, [birdBottom, registerState.showNav])
+
+    //start first obstacle
+    useEffect(() => {
+      if (!registerState.showNav) {
+        console.log("first")
+        if (obstaclesLeft > -60) {
+          obstaclesTimerId = setInterval(() => {
+            setObstaclesLeft(obstaclesLeft => obstaclesLeft - 5)
+          }, 30)
+          return () => {
+            clearInterval(obstaclesTimerId)
+          }
+        } else {
           setScore(score => score +1)
-          setObstaclesLeftTwo(screenWidth)
-          setObstaclesNegHeightTwo( - Math.random() * 100)
+          setObstaclesLeft(screenWidth)
+          setObstaclesNegHeight( - Math.random() * 100)
         }
-  }, [obstaclesLeftTwo])
+      }
+    }, [obstaclesLeft, registerState.showNav]);
+
+     //start second obstacle
+    useEffect(() => {
+      if (!registerState.showNav) {
+        console.log("second")
+        if (obstaclesLeftTwo > -60) {
+          obstaclesTimerIdTwo = setInterval(() => {
+            setObstaclesLeftTwo(obstaclesLeftTwo => obstaclesLeftTwo - 5)
+          }, 30)
+            return () => {
+              clearInterval(obstaclesTimerIdTwo)
+            }
+        } else {
+            setScore(score => score +1)
+            setObstaclesLeftTwo(screenWidth)
+            setObstaclesNegHeightTwo( - Math.random() * 100)
+          }
+      }
+    }, [obstaclesLeftTwo, registerState.showNav]);
 
     //check for collisions
     useEffect(() => {
-      if (
-        ((birdBottom < (obstaclesNegHeight + obstacleHeight + 30) ||
-        birdBottom > (obstaclesNegHeight + obstacleHeight + gap -30)) &&
-        (obstaclesLeft > screenWidth/2 -30 && obstaclesLeft < screenWidth/2 + 30 )
-        )
-        || 
-        ((birdBottom < (obstaclesNegHeightTwo + obstacleHeight + 30) ||
-        birdBottom > (obstaclesNegHeightTwo + obstacleHeight + gap -30)) &&
-        (obstaclesLeftTwo > screenWidth/2 -30 && obstaclesLeftTwo < screenWidth/2 + 30 )
-        )
-        ) 
-        {
-        gameOver()
+      if (!registerState.showNav) {
+        console.log("collission")
+        if (
+          ((birdBottom < (obstaclesNegHeight + obstacleHeight + 30) ||
+          birdBottom > (obstaclesNegHeight + obstacleHeight + gap -30)) &&
+          (obstaclesLeft > screenWidth/2 -30 && obstaclesLeft < screenWidth/2 + 30 )
+          )
+          || 
+          ((birdBottom < (obstaclesNegHeightTwo + obstacleHeight + 30) ||
+          birdBottom > (obstaclesNegHeightTwo + obstacleHeight + gap -30)) &&
+          (obstaclesLeftTwo > screenWidth/2 -30 && obstaclesLeftTwo < screenWidth/2 + 30 )
+          )
+          ) 
+          {
+          gameOver()
+        }
       }
     })
+
+
+
+
+
+   const jump = () => {
+      if (!isGameOver && (birdBottom < screenHeight)) {
+        setBirdBottom(birdBottom => birdBottom + 50)
+      }
+    }
 
     const gameOver = () => {
       clearInterval(gameTimerId)
@@ -110,13 +311,36 @@ export default function App() {
     }
   
   return (
-    true
+    registerState.showNav
     ? <View style={{
         position: "absolute",
         width: "100%",
         height: "100%",
-        backgroundColor: "red",
-      }}></View>
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "green",
+      }}>
+        { registerState.showMsg && <Text style={{
+          width: "100%",
+          color: "red",
+          textAlign: "center",
+          backgroundColor: registerState.color
+        }}>{registerState.msg}</Text> }
+        <TextInput
+          onChangeText={(text => userName = text)}
+          placeholder="User Name"
+          style={{
+            width: "80%",
+            fontSize: 30,
+            textAlign: "center",
+            borderWidth: 3,
+            borderColor: "gray",
+            backgroundColor: "white",
+          }}></TextInput>
+          <Button 
+            onPress={() => register()}
+            title="Enter baby"></Button>
+      </View>
     : <TouchableWithoutFeedback onPress={jump}>
       <View style={styles.container}>
         {
@@ -162,3 +386,6 @@ const styles = StyleSheet.create({
     backgroundColor: 'red',
   },
 })
+
+
+export { firebase };
